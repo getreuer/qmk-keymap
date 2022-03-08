d2d414afb92f04fb987baeeaea9064b935ba04ab
Pascal Getreuer <getreuer@google.com>
2022-03-07 20:12:44 -0800

    Caps Word: adjust default press behavior.
    Adjusts the default press behavior so that:
    
     * KC_MINS continues Caps Word *with* shifting to type underscores.
    
     * KC_DEL continues Caps Word without shifting.

--- features/caps_word.c
+++ features/caps_word.c
@@ -133,13 +133,14 @@ __attribute__((weak)) bool caps_word_press_user(uint16_t keycode) {
   switch (keycode) {
     // Keycodes that continue Caps Word, with shift applied.
     case KC_A ... KC_Z:
+    case KC_MINS:
       add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
       return true;
 
     // Keycodes that continue Caps Word, without shifting.
     case KC_1 ... KC_0:
     case KC_BSPC:
-    case KC_MINS:
+    case KC_DEL:
     case KC_UNDS:
       return true;
 
--- features/caps_word.h
+++ features/caps_word.h
@@ -103,13 +103,14 @@ void caps_word_set_user(bool active);
 //     switch (keycode) {
 //       // Keycodes that continue Caps Word, with shift applied.
 //       case KC_A ... KC_Z:
+//       case KC_MINS:
 //         add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to the next key.
 //         return true;
 //
 //       // Keycodes that continue Caps Word, without shifting.
 //       case KC_1 ... KC_0:
 //       case KC_BSPC:
-//       case KC_MINS:
+//       case KC_DEL:
 //       case KC_UNDS:
 //         return true;
 //
