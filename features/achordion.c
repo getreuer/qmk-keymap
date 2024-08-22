e238f01534f97caf5f25114ecea286cdfff901d0
Pascal Getreuer <getreuer@google.com>
2024-08-21 23:01:11 -0700

    Achordion: Fix eager mods + Caps Word edge case.
    Since eager mods bypass normal event handling, eager Shift does not work
    with `CAPS_WORD_INVERT_ON_SHIFT`. So if this option is enabled, we don't
    apply Shift eagerly when Caps Word is on.
    
    Shift continues to work eagerly while Caps Word is off. Particularly,
    the main use case for eager mods is while using the mouse, and Caps Word
    is typically off during that time.

--- features/achordion.c
+++ features/achordion.c
@@ -175,7 +175,14 @@ bool process_achordion(uint16_t keycode, keyrecord_t* record) {
 
         if (is_mt) {  // Apply mods immediately if they are "eager."
           const uint8_t mod = mod_config(QK_MOD_TAP_GET_MODS(keycode));
-          if (achordion_eager_mod(mod)) {
+          if (
+#if defined(CAPS_WORD_ENABLE) && defined(CAPS_WORD_INVERT_ON_SHIFT)
+              // Since eager mods bypass normal event handling, eager Shift does
+              // not work with CAPS_WORD_INVERT_ON_SHIFT. So if this option is
+              // enabled, we don't apply Shift eagerly when Caps Word is on.
+              !(is_caps_word_on() && (mod & MOD_LSFT) != 0) &&
+#endif  // defined(CAPS_WORD_ENABLE) && defined(CAPS_WORD_INVERT_ON_SHIFT)
+              achordion_eager_mod(mod)) {
             eager_mods = mod;
             process_eager_mods_action();
           }
