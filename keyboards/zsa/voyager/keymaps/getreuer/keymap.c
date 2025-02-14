227d8a13e048a2d8d471e25d44041fe94d5235f7
Pascal Getreuer <getreuer@google.com>
2025-02-13 21:31:03 -0800

    Keymap: Don't exempt thumbs from Chordal Hold.

--- keyboards/zsa/voyager/keymaps/getreuer/keymap.c
+++ keyboards/zsa/voyager/keymaps/getreuer/keymap.c
@@ -36,13 +36,13 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
   '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
   '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
   '*'    , 'L'    , 'L'    , 'L'    , 'L'    , 'L'    ,
-                                               '*'    , '*'    ,
+                                               'L'    , 'L'    ,
 
                     '*'    , '*'    , '*'    , '*'    , '*'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
                     'R'    , 'R'    , 'R'    , 'R'    , 'R'    , '*'    ,
-           '*'    , '*'
+           'R'    , 'R'
 );
 #endif  // CHORDAL_HOLD
 
